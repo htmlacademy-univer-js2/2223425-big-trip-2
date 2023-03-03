d4d0f022f38a74395dbe7e78a33e54191a7be162
nikufim <nufimcev2@gmail.com>
2023-03-03 22:39:16 +0500

    1.10

---
+++ src/view/trip-list.js
@@ -0,0 +1,22 @@
+import { createElement } from '../render';
+const createTripListTemplate = () => (
+<ul class="trip-event__list">    
+</ul>
+    );
+    
+    class TripList {
+        getTemplate() {
+            return createFilterTemplate();
+        }
+        getElement() {
+            if (!this.element) {
+                this.element = createElement(this.getTemplate());
+            }
+            return this.element;
+    
+        }
+        removeElement() {
+            this.element = null
+        }
+    }
+    export default TripList
